d112e8330c2df54303d1de630965542b48b4195f
abdulla adel essa <abdalla.essa@moselaymd.com>
2015-08-24 15:53:46 +0200

    first commit

---
+++ vuforiaSampleApp/app/src/main/java/com/qualcomm/vuforia/samples/moselay/viewToGlRenderer/GLRenderable.java
@@ -0,0 +1,9 @@
+package com.qualcomm.vuforia.samples.moselay.viewToGlRenderer;
+
+/**
+ * Created by user on 5/4/15.
+ */
+public interface GLRenderable {
+
+    void setViewToGLRenderer(ViewToGLRenderer viewToGLRenderer);
+}
