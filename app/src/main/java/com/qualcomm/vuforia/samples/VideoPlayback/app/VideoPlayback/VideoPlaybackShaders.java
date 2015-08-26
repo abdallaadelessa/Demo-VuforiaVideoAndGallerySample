07d8b3ab6edf50e86a5a0ce5a30826871fd7db2f
abdulla adel essa <abdalla.essa@moselaymd.com>
2015-08-26 17:37:51 +0200

    add border to video on texture

--- app/src/main/java/com/qualcomm/vuforia/samples/VideoPlayback/app/VideoPlayback/VideoPlaybackShaders.java
+++ app/src/main/java/com/qualcomm/vuforia/samples/VideoPlayback/app/VideoPlayback/VideoPlaybackShaders.java
@@ -7,22 +7,9 @@ and other countries. Trademarks of QUALCOMM Incorporated are used with permissio
 
 package com.qualcomm.vuforia.samples.VideoPlayback.app.VideoPlayback;
 
-public class VideoPlaybackShaders
-{
-    
-    public static final String VIDEO_PLAYBACK_VERTEX_SHADER = " \n"
-        + "attribute vec4 vertexPosition; \n"
-        + "attribute vec4 vertexNormal; \n"
-        + "attribute vec2 vertexTexCoord; \n" 
-        + "varying vec2 texCoord; \n"
-        + "varying vec4 normal; \n"
-        + "uniform mat4 modelViewProjectionMatrix; \n" 
-        + "\n"
-        + "void main() \n" 
-        + "{ \n"
-        + "   gl_Position = modelViewProjectionMatrix * vertexPosition; \n"
-        + "   normal = vertexNormal; \n" + "   texCoord = vertexTexCoord; \n"
-        + "} \n";
+public class VideoPlaybackShaders {
+
+    public static final String VIDEO_PLAYBACK_VERTEX_SHADER = " \n" + "attribute vec4 vertexPosition; \n" + "attribute vec4 vertexNormal; \n" + "attribute vec2 vertexTexCoord; \n" + "varying vec2 texCoord; \n" + "varying vec4 normal; \n" + "uniform mat4 modelViewProjectionMatrix; \n" + "\n" + "void main() \n" + "{ \n" + "   gl_Position = modelViewProjectionMatrix * vertexPosition; \n" + "   normal = vertexNormal; \n" + "   texCoord = vertexTexCoord; \n" + "} \n";
     
     /*
      * 
@@ -36,15 +23,23 @@ public class VideoPlaybackShaders
      * extension declaration (first line). The second is the type of the
      * texSamplerOES uniform.
      */
-    
-    public static final String VIDEO_PLAYBACK_FRAGMENT_SHADER = " \n"
-        + "#extension GL_OES_EGL_image_external : require \n"
-        + "precision mediump float; \n" 
-        + "varying vec2 texCoord; \n"
-        + "uniform samplerExternalOES texSamplerOES; \n" + " \n"
-        + "void main() \n" 
-        + "{ \n"
-        + "   gl_FragColor = texture2D(texSamplerOES, texCoord); \n" 
-        + "} \n";
-    
+
+    public static final String VIDEO_PLAYBACK_FRAGMENT_SHADER = "#extension GL_OES_EGL_image_external : require \n" +
+            "precision mediump float; \n" +
+            "varying vec2 texCoord; \n" +
+            "uniform samplerExternalOES texSamplerOES; \n" +
+            "void main() \n" +
+            "{ \n" +
+            "   vec4 texColor = texture2D(texSamplerOES, texCoord); \n" +
+            "   vec3 borderColor = vec3(1.0 ,1.0, 1.0); \n" +
+            "   float u = 2.0*abs(texCoord.x - 0.5);\n" +
+            "   float v = 2.0*abs(texCoord.y - 0.5);\n" +
+            "   gl_FragColor = texColor; \n" +
+            "   float borderWidth = 0.01; \n" +
+            "   float borderHeight = 0.01; \n" +
+            "   float contentWidth = 1.0 - borderWidth; \n" +
+            "   float contentHeight = 1.0 - borderHeight; \n" +
+            "   gl_FragColor.rgb = ((u > contentWidth) || (v > contentHeight)) ? borderColor : gl_FragColor.rgb; \n" +
+            "} \n";
+
 }
--- app/src/main/java/com/qualcomm/vuforia/samples/VuforiaSamples/app/ObjectRecognition/ObjectTargetUtils.java
+++ app/src/main/java/com/qualcomm/vuforia/samples/VuforiaSamples/app/ObjectRecognition/ObjectTargetUtils.java
@@ -1,5 +1,10 @@
 package com.qualcomm.vuforia.samples.VuforiaSamples.app.ObjectRecognition;
 
+import android.opengl.GLES20;
+
+import com.qualcomm.vuforia.samples.SampleApplication.utils.LineShaders;
+import com.qualcomm.vuforia.samples.SampleApplication.utils.SampleUtils;
+
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -28,5 +33,55 @@ public class ObjectTargetUtils {
 
         return bb;
 
+    }
+    public static Buffer fillBuffer(double[] array) {
+        // Convert to floats because OpenGL doesnt work on doubles, and manually
+        // casting each input value would take too much time.
+        ByteBuffer bb = ByteBuffer.allocateDirect(4 * array.length); // each
+        // float
+        // takes 4
+        // bytes
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        for(double d : array)
+            bb.putFloat((float) d);
+        bb.rewind();
+
+        return bb;
+
+    }
+
+
+    public static void drawBorder(double[] vbVertices, float[] modelViewProjection) {
+
+//         int vbShaderProgramID = 0;
+//         int vbVertexHandle = 0;
+//         int lineOpacityHandle = 0;
+//         int lineColorHandle = 0;
+//         int mvpMatrixButtonsHandle = 0;
+//        vbShaderProgramID = SampleUtils.createProgramFromShaderSrc(LineShaders.LINE_VERTEX_SHADER, LineShaders.LINE_FRAGMENT_SHADER);
+//        mvpMatrixButtonsHandle = GLES20.glGetUniformLocation(vbShaderProgramID, "modelViewProjectionMatrix");
+//        vbVertexHandle = GLES20.glGetAttribLocation(vbShaderProgramID, "vertexPosition");
+//        lineOpacityHandle = GLES20.glGetUniformLocation(vbShaderProgramID, "opacity");
+//        lineColorHandle = GLES20.glGetUniformLocation(vbShaderProgramID, "color");
+//        // Render frame around button
+//        GLES20.glUseProgram(vbShaderProgramID);
+//        GLES20.glVertexAttribPointer(vbVertexHandle, 3, GLES20.GL_FLOAT, false, 0, fillBuffer(vbVertices));
+//        GLES20.glEnableVertexAttribArray(vbVertexHandle);
+//        GLES20.glUniform1f(lineOpacityHandle, 1.0f);
+//        GLES20.glUniform3f(lineColorHandle, 1.0f, 1.0f, 1.0f);
+//        GLES20.glLineWidth(10);
+//        GLES20.glUniformMatrix4fv(mvpMatrixButtonsHandle, 1, false, modelViewProjection, 0);
+//        // We multiply by 8 because that's the number of vertices per
+//        // button
+//        // The reason is that GL_LINES considers only pairs. So some
+//        // vertices
+//        // must be repeated.
+//        GLES20.glDrawArrays(GLES20.GL_LINES, 0, 16);
+//
+//        SampleUtils.checkGLError("VirtualButtons drawButton");
+//
+//        GLES20.glDisableVertexAttribArray(vbVertexHandle);
+//
+//        GLES20.glUseProgram(0);
     }
 }
